35378fd019c3f5ec88cffbf5178be0e74de3679a
sporeball <sporeballdev@gmail.com>
2025-05-02 10:42:15 -0400

    cmd/key: multi word

--- commands/key.js
+++ commands/key.js
@@ -1,5 +1,5 @@
 import cmd from "../command.js";
 
 export default class key extends cmd {
-  static self = cmd.gen('key').on('call', o => o.v[o.a.at(0)]).commit();
+  static self = cmd.gen('key').on('call', o => o.v[o.i]).commit();
 }
